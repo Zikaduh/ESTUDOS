2fbb5d305dd7ad203c53a11ec94504f60755ff4c
João Vitor <jv.alves97@hotmail.com>
2021-11-17 20:11:24 -0300

    Create Exercicio1.js
    exercício 1 feito.

---
+++ Javascript Exercicios/Exercicio1.js
@@ -0,0 +1,10 @@
+//	Vamos criar variaveis usando let, const e var, atribuir valores,
+// identificar comportamentos e saídas quando tentarmos recuperar.
+
+
+let a = 5; // são guardada no escopo local e pode ser alterada
+var b = 10; // são guardada no escopo global e pode ser alterada
+const c = 15; // funciona quase que nem um let, mas tem que ser inicializada e não pode ser reatribuida.
+
+console.log(a + b + c)
+
